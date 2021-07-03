13af7bba8a3207f73ad9147c3b079253be46c179
Arina Petrova <petrova.arina.s@yandex.ru>
2021-07-03 12:00:48 +0300

    Corrected postprocessing.py

--- postprocessing.py
+++ postprocessing.py
@@ -1,20 +1,42 @@
+from weather import days_with_temp
+
 def city_and_day_with_max_temp(df):
     maximum_temp = float("-inf")
-    for key, value in df.iteritems():
-        if value[0]["maximum"] > maximum_temp:
-            maximum_temp = value[0]["maximum"]
+    for i in df.values:
+        for j in i:
+            if j["maximum"] > maximum_temp:
+                maximum_temp = j["maximum"]
 
     return maximum_temp
 
 
 def city_and_day_with_min_temp(df):
     minimum_temp = float("inf")
-    for key, value in df.iteritems():
-        if value[0]["minimum"] < minimum_temp:
-            minimum_temp = value[0]["minimum"]
+    for i in df.values:
+        for j in i:
+            if j["minimum"] < minimum_temp:
+                minimum_temp = j["minimum"]
 
     return minimum_temp
 
+def city_with_max_range_of_max_tep(df):
+    maximum_range = float("-inf")
+    for i in df.values:
+        i = [j['maximum'] for j in i]
+        range_temp = max(i) - min(i)
+        if range_temp > maximum_range:
+            maximum_range = range_temp
 
-# print(city_and_day_with_max_temp(days_with_temp))
-# print(city_and_day_with_min_temp(days_with_temp))
+    return maximum_range
+
+def city_and_day_with_max_range_of_min_and_max_temp(df):
+    maximum_range_min_max = float("-inf")
+    for i in df.values:
+        for j in i:
+            range_temp = j['maximum'] - j['minimum']
+            if range_temp > maximum_range_min_max:
+                maximum_range_min_max = round(range_temp, 2)
+
+    return maximum_range_min_max
+
+
